123d595947d1060f4b3ce56ba0bb779858b3c936
SPLoT <mahonen.samu@gmail.com>
2020-03-26 14:09:37 +0200

    Js päivitetty

--- friendlist.js
+++ friendlist.js
@@ -2,15 +2,14 @@ const Nimi = document.querySelector("input");
 const addKaveri = document.querySelector("button");
 const Kaverilista = document.querySelector(".kaverit")
 
-const Friends = [];
+const Friends = ["","Jorma","","Erkki"];
 
 function Lisääkaveri() {
 if(Friends.length < 10) {
 Friends.unshift(Nimi.value);
 Kaverilista.innerHTML +="<br>";
-Kaverilista.innerText += Friends[0]}
+Kaverilista.innerText += Friends[0];}
 else {
 alert("Älä valehtele! et oo niin suosittu.");}
 };
-
 addKaveri.onclick = Lisääkaveri; 
