44562cf7c100cfc7af419cc96c12f83c3ea01909
SPLoT <mahonen.samu@gmail.com>
2020-04-06 09:49:41 +0300

    Estetty tyhjien objektion laittaminen arrayhin

--- friendlist.js
+++ friendlist.js
@@ -2,13 +2,27 @@ const Nimi = document.querySelector("input");
 const addKaveri = document.querySelector("button");
 const Kaverilista = document.querySelector(".kaverit")
 
-const Friends = ["","Jorma","","Erkki"];
+let Friends = [];
+
+function remover() {
+var filtered = Friends.filter(Boolean);
+Friends = filtered;
+console.log(Friends);}
+
+function linebreak() {
+    Kaverilista.innerHTML +="<br>";
+}
 
 function Lisääkaveri() {
 if(Friends.length < 10) {
+remover();
 Friends.unshift(Nimi.value);
-Kaverilista.innerHTML +="<br>";
-Kaverilista.innerText += Friends[0];}
+var filtered = Friends.filter(Boolean);
+Friends = filtered
+console.log(Friends)
+Kaverilista.innerText = Friends;
+console.log(Friends)
+;}
 else {
 alert("Älä valehtele! et oo niin suosittu.");}
 };
