c12e242287171b28d3049d2600b2611444d919ed
SPLoT <mahonen.samu@gmail.com>
2020-04-07 12:23:57 +0300

    Rivin vaihto backportattu ja korjattu

--- friendlist.js
+++ friendlist.js
@@ -3,16 +3,29 @@ const addKaveri = document.querySelector("button");
 const Kaverilista = document.querySelector(".kaverit")
 
 let Friends = [];
+let Printable
+
+function linebreaker() {
+    let separator=Friends.join(":muC4piPojMpxTF1yl0B4sRHxC13FvcL6UeFNUFzAKH3fvXKNnxTNKGotJu4LrqjN5394zfg1MxOcHsxMqDLJT8Qf3PU2Cc9QtdjDhlICev2ZcfUolIVZYm9XUhr4WZnXmrhSeVm1otEIU1Gi7EIomrYz3sHCS9k72qoHP6N7cwMHOagWrDPbm6L3r4fHnGM4mGumP7At5iW1pZJMCsRuI01IzczK1on4XmzufsWmBAX7jcAo6XaZS0Z52akvk6Zq:");
+    Printable = separator
+}
+
+function LinebreakerSafe() {
+    var res = Kaverilista.innerHTML.replace(/:muC4piPojMpxTF1yl0B4sRHxC13FvcL6UeFNUFzAKH3fvXKNnxTNKGotJu4LrqjN5394zfg1MxOcHsxMqDLJT8Qf3PU2Cc9QtdjDhlICev2ZcfUolIVZYm9XUhr4WZnXmrhSeVm1otEIU1Gi7EIomrYz3sHCS9k72qoHP6N7cwMHOagWrDPbm6L3r4fHnGM4mGumP7At5iW1pZJMCsRuI01IzczK1on4XmzufsWmBAX7jcAo6XaZS0Z52akvk6Zq:/g,"<br>")
+    Kaverilista.innerHTML = res
+}
 
 function remover() {
-var filtered = Friends.filter(Boolean);
+var filtered = Friends.filter(Friends => Friends.length > 2);
 Friends = filtered;}
 
 function Lisääkaveri() {
 if(Friends.length < 10) {
-remover();
 Friends.unshift(Nimi.value);
-Kaverilista.innerText = Friends+" ";
+remover();
+linebreaker();
+Kaverilista.innerText = Printable;
+LinebreakerSafe();
 Nimi.value="";
 ;}
 else {
