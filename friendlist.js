60d71e5f040e908e45a3997c1a2412c01185b12b
SPLoT <mahonen.samu@gmail.com>
2020-04-07 11:31:00 +0300

    Valmistelty versio 2js

--- friendlist.js
+++ friendlist.js
@@ -6,43 +6,50 @@ const test = document.querySelector("#test")
 const Kaverilista = document.querySelector("#kaverit");
 
 
-var Friends = [];
+let Friends = [];
+let Printable
 
-function linebreak() {
-    var str = document.getElementById("kaverit").innerHTML; 
-    var res = str.replace(/,/g, "<br>");
-    document.getElementById("kaverit").innerHTML = res;
-  }
+function linebreaker() {
+    let separator=Friends.join("  :  ");
+    Printable = separator
+}
+
 
 function remover() {
-var filtered = Friends.filter(Boolean);
+var filtered = Friends.filter(Friends => Friends.length > 2);
 Friends = filtered;}
 
 function Lisääkaveri() {
-remover();
 Friends.unshift(Nimi.value);
-Kaverilista.innerHTML.replace(",",".");
-Kaverilista.innerText = Friends;
-console.log(Friends);
+remover();
+linebreaker();
+
+Kaverilista.innerText = Printable
 Nimi.value="";
-linebreak
-console.log(Kaverilista.innerHTML)
+
 }
 
 
 function del() {
     Friends.shift();
-    console.log(Friends);
-    Kaverilista.innerText = Friends;
+    console.log(Friends);    linebreaker();
+    Kaverilista.innerText = Printable;
 }
 
 function sort() {
     Friends.sort();
-    console.log(Friends);
-    Kaverilista.innerText = Friends;
+    linebreaker();
+    Kaverilista.innerText = Printable;
+}
+
+function testi() {
+    var res = Kaverilista.innerHTML.replace(":","<br>")
+    Kaverilista.innerHTML = res
+    console.log(Kaverilista.outerHTML);
 }
 
 console.log(Friends)
 sortfriends.onclick = sort
 delKaveri.onclick = del
 addKaveri.onclick = Lisääkaveri
+test.onclick = testi
