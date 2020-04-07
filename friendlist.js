f58eb460af487f6d14d2d0eac38ec9903003d937
SPLoT <mahonen.samu@gmail.com>
2020-04-07 11:59:20 +0300

    Rivin vaihto valmis

--- friendlist.js
+++ friendlist.js
@@ -10,10 +10,9 @@ let Friends = [];
 let Printable
 
 function linebreaker() {
-    let separator=Friends.join("  :  ");
+    let separator=Friends.join(":muC4piPojMpxTF1yl0B4sRHxC13FvcL6UeFNUFzAKH3fvXKNnxTNKGotJu4LrqjN5394zfg1MxOcHsxMqDLJT8Qf3PU2Cc9QtdjDhlICev2ZcfUolIVZYm9XUhr4WZnXmrhSeVm1otEIU1Gi7EIomrYz3sHCS9k72qoHP6N7cwMHOagWrDPbm6L3r4fHnGM4mGumP7At5iW1pZJMCsRuI01IzczK1on4XmzufsWmBAX7jcAo6XaZS0Z52akvk6Zq:");
     Printable = separator
 }
-
 
 function remover() {
 var filtered = Friends.filter(Friends => Friends.length > 2);
@@ -23,33 +22,31 @@ function Lisääkaveri() {
 Friends.unshift(Nimi.value);
 remover();
 linebreaker();
-
-Kaverilista.innerText = Printable
+Kaverilista.innerText = Printable;
+testi();
 Nimi.value="";
-
 }
 
 
 function del() {
     Friends.shift();
-    console.log(Friends);    linebreaker();
+    linebreaker();
     Kaverilista.innerText = Printable;
+    testi();
 }
 
 function sort() {
     Friends.sort();
     linebreaker();
     Kaverilista.innerText = Printable;
+    testi();
 }
 
 function testi() {
-    var res = Kaverilista.innerHTML.replace(":","<br>")
+    var res = Kaverilista.innerHTML.replace(/:muC4piPojMpxTF1yl0B4sRHxC13FvcL6UeFNUFzAKH3fvXKNnxTNKGotJu4LrqjN5394zfg1MxOcHsxMqDLJT8Qf3PU2Cc9QtdjDhlICev2ZcfUolIVZYm9XUhr4WZnXmrhSeVm1otEIU1Gi7EIomrYz3sHCS9k72qoHP6N7cwMHOagWrDPbm6L3r4fHnGM4mGumP7At5iW1pZJMCsRuI01IzczK1on4XmzufsWmBAX7jcAo6XaZS0Z52akvk6Zq:/g,"<br>")
     Kaverilista.innerHTML = res
-    console.log(Kaverilista.outerHTML);
 }
 
-console.log(Friends)
 sortfriends.onclick = sort
 delKaveri.onclick = del
 addKaveri.onclick = Lisääkaveri
-test.onclick = testi
