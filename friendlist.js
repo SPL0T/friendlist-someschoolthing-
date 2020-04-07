f2a6fe1d0dea696c166f926026ec534baea8f71f
SPLoT <mahonen.samu@gmail.com>
2020-04-07 12:16:24 +0300

    Viimeinen julkaisu ellen keksi enaa muuta

--- friendlist.js
+++ friendlist.js
@@ -23,7 +23,7 @@ Friends.unshift(Nimi.value);
 remover();
 linebreaker();
 Kaverilista.innerText = Printable;
-testi();
+LinebreakerSafe();
 Nimi.value="";
 }
 
@@ -32,17 +32,17 @@ function del() {
     Friends.shift();
     linebreaker();
     Kaverilista.innerText = Printable;
-    testi();
+    LinebreakerSafe();
 }
 
 function sort() {
     Friends.sort();
     linebreaker();
     Kaverilista.innerText = Printable;
-    testi();
+    LinebreakerSafe();
 }
 
-function testi() {
+function LinebreakerSafe() {
     var res = Kaverilista.innerHTML.replace(/:muC4piPojMpxTF1yl0B4sRHxC13FvcL6UeFNUFzAKH3fvXKNnxTNKGotJu4LrqjN5394zfg1MxOcHsxMqDLJT8Qf3PU2Cc9QtdjDhlICev2ZcfUolIVZYm9XUhr4WZnXmrhSeVm1otEIU1Gi7EIomrYz3sHCS9k72qoHP6N7cwMHOagWrDPbm6L3r4fHnGM4mGumP7At5iW1pZJMCsRuI01IzczK1on4XmzufsWmBAX7jcAo6XaZS0Z52akvk6Zq:/g,"<br>")
     Kaverilista.innerHTML = res
 }
